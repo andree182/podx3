6499e51c03558938d135929b27724c79a6155ffb
Andrej Krutak <dev@andree.sk>
2015-05-03 21:17:46 +0200

    read/parse the serial code cleanup

--- pypodx3.py
+++ pypodx3.py
@@ -23,15 +23,16 @@ USAGE:
 
 import sys, time, math, array, struct, string
 import usb
+import struct
 
 def formathex(buffer):
-	""" return buffer content as hex formatted bytes """
-	buf2 = []
-	if buffer is None or len(buffer) == 0:
-		return ""
-	for item in buffer:
-		buf2.append("%02X"%(item))
-	return string.join(buf2," ")
+    """ return buffer content as hex formatted bytes """
+    buf2 = []
+    if buffer is None or len(buffer) == 0:
+        return ""
+    for item in buffer:
+        buf2.append("%02X"%(item))
+    return string.join(buf2," ")
 
 class DeviceDescriptor(object) :
     def __init__(self, vendor_id, product_id, interface_id) :
@@ -82,32 +83,40 @@ class POD():
             print >> sys.stderr, "err:",err
     
     def getConf(self):
-	""" return device selected configuration """
+        """ return device selected configuration """
         reqType = 0x80
         resp = self.handle.controlMsg(reqType, usb.REQ_GET_CONFIGURATION, 1, value=0x0000, index=0x00)
         if len(resp) > 0:
-		print "configuration:", resp
-	return resp[0]
+            print "configuration:", resp
+        return resp[0]
     
-        def getIf(self):
-		""" return device selected interface """
-        	reqType = 0x80
-        	resp = self.handle.controlMsg(reqType, usb.REQ_GET_CONFIGURATION, 1, value=0x0000, index=0x00)
-        	if len(resp) > 0:
-			print "configuration:", resp
-		return resp[0]
-
+    def getIf(self):
+        """ return device selected interface """
+        reqType = 0x80
+        resp = self.handle.controlMsg(reqType, usb.REQ_GET_CONFIGURATION, 1, value=0x0000, index=0x00)
+        if len(resp) > 0:
+            print "configuration:", resp
+        return resp[0]
     
     def setguitarmic(self):
         """ set guitar/mic mode """
         try:
-          print "g/m:start bulk write"
-          buf = [0x18, 0x00, 0x01, 0x00, 0x05, 0x00, 0x0A, 0x40, 0x01, 0x03, 0x00, 0x16, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00]
-          sent = self.handle.bulkWrite(0x01,buf)
-          print "past write", sent
+            print "g/m:start bulk write"
+            buf = [0x18, 0x00, 0x01, 0x00, 0x05, 0x00, 0x0A, 0x40, 0x01, 0x03, 0x00, 0x16, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00]
+            sent = self.handle.bulkWrite(0x01,buf)
+            print "past write", sent
         except Exception, e:
-          print "exception in bulkWrite:"
-          print e
+            print "exception in bulkWrite:"
+            print e
+
+    def readData(self, dataLen, addr):
+        resp = self.handle.controlMsg(0x40, 0x67, 0, value=(dataLen << 8) | 0x21, index=addr)
+        print "resp (ACK)", formathex(resp)
+        resp = self.handle.controlMsg(0xC0, 0x67, 1, value=0x12, index=0x00)
+        print "resp (x12)", formathex(resp)
+        resp = self.handle.controlMsg(0xC0, 0x67, dataLen, value=0x13, index=0x00)
+        print "resp (x13)", formathex(resp)
+        return resp
 
     def init(self):
         """ pod initialization - trying to do the same things the original driver does during X3 startup """
@@ -121,12 +130,8 @@ class POD():
         #a sequence i don;t understand yet
         bytes = [0x00, 0x08, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38, 0x40, 0x48, 0x50, 0x58, 0x60, 0x68, 0x70, 0x78, 0x80]
         for item in bytes:
-            resp = self.handle.controlMsg(0x40, 0x67, 0, value=0x0821, index=0xF000 | item)
-            print "resp (ACK)", formathex(resp)
-            resp = self.handle.controlMsg(0xC0, 0x67, 1, value=0x12, index=0x00)
-            print "resp (x12)", formathex(resp)
-            resp = self.handle.controlMsg(0xC0, 0x67, 8, value=0x13, index=0x00)
-            print "resp (x13)", formathex(resp)
+            # This looks exactly like $linuxdrv/driver.c/line6_read_data()
+            self.readData(8, 0xF000 | item)
             resp = self.handle.controlMsg(0x40, 0x67, 0, value=0x0201, index=0x02)
             print "resp (x201)", formathex(resp)
 
@@ -149,9 +154,6 @@ class POD():
         resp = self.handle.bulkRead(0x1, 0x40)
         print "resp", formathex(resp)
 
-    def start(self):
-        pass
-
     def close(self):  
         """ Release device interface """
         try:
@@ -160,90 +162,44 @@ class POD():
         except Exception, err:
             print >> sys.stderr, err 
 
-
     def setparam(self, paramnum, value_percent):
         """
         set channel parameter value
-	most , if not all, values are in range 0x00 00 00 00 - 0x3f 80 00 00
-
+        most , if not all, values are in range 0x00 00 00 00 - 0x3f 80 00 00
         """
         buf = [0x1C, 0x00, 0x01, 0x00, 0x06, 0x00, 0x0A, 0x40, 0x01, 0x03, 0x00, 0x15, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x10, 0x3F, 0x00, 0x00, 0x00, 0x00]
         maxval = 0x3f800000
         val = (maxval/100) * value_percent
         try:
-          #help(self.handle.bulkWrite)
-          buf[0x18] = paramnum
-          paramval = struct.pack('i', val)
-          buf[0x1c] = ord(paramval[0])
-          buf[0x1d] = ord(paramval[1])
-          buf[0x1e] = ord(paramval[2])
-          buf[0x1f] = ord(paramval[3])
-          print 'val=',paramval, 'paramnum=', paramnum
-          print formathex(buf)
-	  print "start bulk write"
-          sent = self.handle.bulkWrite(1,buf,100)
-	  print "past write", sent
-          #sent = self.handle.bulkWrite(0x01,buf,100)
-	  #print "past write2", sent
-	  #this message can be see in usb dumps everythime after changing param value,
-	  #however it appears only once after cranking param up and down constantly
-          resp = self.handle.controlMsg(0xc0, 0x67, 0x03, value=0x11, index=0x00)
-	  print "resp", resp
+            #help(self.handle.bulkWrite)
+            buf[0x18] = paramnum
+            paramval = struct.pack('i', val)
+            buf[0x1c] = ord(paramval[0])
+            buf[0x1d] = ord(paramval[1])
+            buf[0x1e] = ord(paramval[2])
+            buf[0x1f] = ord(paramval[3])
+            print 'val=',paramval, 'paramnum=', paramnum
+            print formathex(buf)
+            print "start bulk write"
+            sent = self.handle.bulkWrite(1,buf,100)
+            print "past write", sent
+            #sent = self.handle.bulkWrite(0x01,buf,100)
+            #print "past write2", sent
+            #this message can be see in usb dumps everythime after changing param value,
+            #however it appears only once after cranking param up and down constantly
+            resp = self.handle.controlMsg(0xc0, 0x67, 0x03, value=0x11, index=0x00)
+            print "resp", resp
         except Exception, e:
-          print "exception in bulkWrite:",e
+            print "exception in bulkWrite:",e
 
-    def get_serial_number(self):
-        """ get POD serial number """
-        try:
-          print "get serial number"
-          reqbuffer = [0] * 8
-
-	  #serial number goes here
-          reqType = 0xc0 #usb.TYPE_CLASS | usb.RECIP_INTERFACE | usb.ENDPOINT_OUT
-          resp = self.handle.controlMsg(reqType, 0x67, 4, value=0x0013, index=0x00)
-          if len(resp) > 0:
-              print "serial number:", formathex(resp), " -> ",struct.unpack('I',chr(resp[0])+chr(resp[1])+chr(resp[2])+chr(resp[3]))[0]
-
-          #todo
-          reqType = 0x40 #usb.TYPE_CLASS | usb.RECIP_INTERFACE | usb.ENDPOINT_OUT
-          resp = self.handle.controlMsg(reqType, 0x67, 0, value=0x11, index=0)
-          if len(resp) > 0:
-              print "resp1", formathex(resp)
-
-          #todo - probably firmware version
-          reqType = 0xc0 #usb.TYPE_CLASS | usb.RECIP_INTERFACE | usb.ENDPOINT_OUT
-          resp = self.handle.controlMsg(reqType, 0x67, 3, value=0x11, index=0)
-          if len(resp) > 0:
-              print "resp2", formathex(resp)
-
-          #todo
-          reqType = 0x40 #usb.TYPE_CLASS | usb.RECIP_INTERFACE | usb.ENDPOINT_OUT
-          resp = self.handle.controlMsg(reqType, 0x67, 0, value=0x0421, index=0x80d0)
-          if len(resp) > 0:
-              print "resp3", formathex(resp)
-
-          #todo
-          reqType = 0x40 #usb.TYPE_CLASS | usb.RECIP_INTERFACE | usb.ENDPOINT_OUT
-          resp = self.handle.controlMsg(reqType, 0x67, 0, value=0x0421, index=0x80d0)
-          if len(resp) > 0:
-              print "resp4", formathex(resp)
-
-          #todo
-          reqType = 0xc0 #usb.TYPE_CLASS | usb.RECIP_INTERFACE | usb.ENDPOINT_OUT
-          resp = self.handle.controlMsg(reqType, 0x67, 1, value=0x0012, index=0x00)
-          if len(resp) > 0:
-              print "resp5", formathex(resp)
-
-        except Exception, e:
-          print e
-
+    def get_serial_number2(self):
+        d = self.readData(4, 0x80d0)
+        print "POD Serial: %d" % (struct.unpack('<I', ''.join([chr(i) for i in d])))
 
 pod = POD()
 pod.open()
 pod.init()
-#pod.get_serial_number()
-pod.start()
+pod.get_serial_number2()
 #pod.setparam(5, 10)
 #pod.setguitarmic()
 pod.close()
-
