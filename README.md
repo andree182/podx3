a553d5cedd793dfab1346702f4e47769f048b54b
Andrej Krutak <dev@andree.sk>
2015-10-25 16:39:53 +0100

    Cleanup + make the receival/merging of messages work, update readme

--- README.md
+++ README.md
@@ -1,5 +1,58 @@
-# podx3
+podx3
+=====
 
-Linux kernel now contains support for some Line6 PODs, but not X3, and apparently also not for settings manipulation.
+Linux kernel now contains support for some Line6 PODs, but not X3, and
+apparently also not for settings manipulation.
 
-This code is partly based on what was already done in http://sourceforge.net/p/line6linux/ (esp. the x3 branch). Some userspace stuff+docs will be dumped here - linux driver modifications will hopefully be kept in linux kernel eventually :-)
+This code is partly based on what was already done in
+http://sourceforge.net/p/line6linux/ (esp. the x3 branch). Some userspace
+stuff+docs will be dumped here - linux driver modifications will hopefully be
+kept in linux kernel eventually :-)
+
+
+
+pypodx3.py
+==========
+
+This tool can be used to dump the bulk commands used to configure the pod.
+You'll need python + pyusb for it.
+
+Read/write command format seem to be (almost) the same.
+
+
+
+POD bulk message format
+=======================
+
+Apparently, pod bulk messages consist of header and body.
+
+
+Header
+------
+At least the incoming messages may not be complete. The header byte-format is:
+ContentsLength ?? Flags ??
+
+Flags is 0x01 for first packet, 0x04 for followup packets (to be appended to
+previous buffer).
+
+
+Contents
+--------
+Standard floats are used, e.g.
+
+06 00 0A 03 00 40 00 15 00 00 00 00 00 00 03 00 01 00 00 00 05 00 10 3F DD 74 53 3F
+
+for setting "Tone volume".
+
+The detailed format is "decrypted" to a great degree, will be described more
+closely later (or on request, whichever comes first :-) ) - but it's pretty
+straight-forward in the end, no cypher/checksums seem to be involved.
+
+
+Audio transfer
+==============
+
+Isochronous messages seem to be in S24_3LE format, 8 channels (input; interleaved
+as 1L1R 2L2R 3L3R 4L4R (?)) / 2 channels (output).
+
+Audio transfer appears to work without the control/bulk setup.
--- pypodx3.py
+++ pypodx3.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
 """
 Simple POD x3 control utility, written to test pod functionality.
-Warning: this code may remove your warranty, erase firmware, eat your dog and so on. Use at your own risk.
-libusb and python bindings are required.
+Warning: this code may remove your warranty, erase firmware, eat your dog and
+so on. Use at your own risk.
+pyusb is required.
 This code is released under GPL v2. Have fun.
 
 NOTE:
-	before using, look at the end of the file and uncomment functon calls you want to use
+	before using, look at the end of the file and uncomment functon calls you
+	want to use
 
 USAGE:
-	Turn on pod
-	remove line6usb kernel module, using rmmod or (better) delete it or disable it.
-	wait few seconds
-	look at the end of this file and comment/uncomment function calls you want to test
-	type python pypodx3.py in console
-	it sometimes doesn't work for the first time, works for the second and then doesn't work until pod is turned off and on again.
-	
-	You can get interactive console: comment pod.close() call and run it using python -i pypodx3.py
-	then you can control pod object
+	1) turn on pod
+	2) look at the end of this file and comment/uncomment function calls you
+	   want to test
+	2.1) it sometimes doesn't work for the first time, works for the second and
+	   then doesn't work until pod is turned off and on again.
+	3) you'll see configuration packets coming as you play with x3 controls
+
+	You can get interactive console: run it using
+	python -i pypodx3.py then you can control pod object...
 
 """
 
 import sys, time, math, array, struct, string
-import usb
+import usb.util, usb.core
 import struct
 import threading
 import signal
@@ -30,136 +32,75 @@ import signal
 def formathex(buffer):
     """ return buffer content as hex formatted bytes """
     buf2 = []
-    if buffer is None or len(buffer) == 0:
-        return ""
+    if not isinstance(buffer, array.array):
+        return "== " + str(buffer)
     for item in buffer:
-        buf2.append("%02X"%(item))
+        buf2.append("%02X" % (item))
     return string.join(buf2," ")
 
-class DeviceDescriptor(object) :
-    def __init__(self, vendor_id, product_id, interface_id) :
-        self.vendor_id = vendor_id
-        self.product_id = product_id
-        self.interface_id = interface_id
-
-    def getDevice(self):
-        """
-        Return the device corresponding to the device descriptor if it is
-        available on a USB bus.  Otherwise, return None.  Note that the
-        returned device has yet to be claimed or opened.
-        """
-        buses = usb.busses()
-        for bus in buses :
-            for device in bus.devices :
-                if device.idVendor == self.vendor_id :
-                    if device.idProduct == self.product_id :
-                        return device
-        print "no device"
-	sys.exit(1)
-
-class POD():
+class POD:
     VENDOR_ID     = 0x0E41   #: Vendor Id
     PRODUCT_ID    = 0x414A   #: Product Id for POD X3 bean
     INTERFACE_ID  = 1        #: The interface we use to talk to the device
-    #BULK_IN_EP   = 0x86     #: Endpoint for Bulk reads
-    #BULK_OUT_EP  = 0x01     #: Endpoint for Bulk writes
+    BULK_IN_EP   = 0x81      #: Endpoint for Bulk reads
+    BULK_OUT_EP  = 0x01      #: Endpoint for Bulk writes
     PACKET_LENGTH = 0x40     #: 64 bytes
-    device_descriptor = DeviceDescriptor(VENDOR_ID, PRODUCT_ID, INTERFACE_ID)
+
+    L6_X3_CTRL = 0x67 # bRequest == 103
+    USB_VENDOR_H2D = 0x40
+    USB_VENDOR_D2H = 0xC0
 
     def __init__(self,) :
-      # The actual device (PyUSB object)
-      self.device = self.device_descriptor.getDevice()
-      # Handle that is used to communicate with device. Setup in L{open}
-      self.handle = None
-   
-    def open(self) :
-        #self.device = self.device_descriptor.getDevice()
-        if not self.device:
-            print >> sys.stderr, "POD isn't plugged in"
-        try:
-            self.handle = self.device.open()
-            #self.handle.detachKernelDriver(0)
-            #self.handle.detachKernelDriver(1)
-            #self.handle.detachKernelDriver(2)
-        except usb.USBError, err:
-            print >> sys.stderr, "err:",err
-    
-    def getConf(self):
-        """ return device selected configuration """
-        reqType = 0x80
-        resp = self.handle.controlMsg(reqType, usb.REQ_GET_CONFIGURATION, 1, value=0x0000, index=0x00)
-        if len(resp) > 0:
-            print "configuration:", resp
-        return resp[0]
-    
-    def getIf(self):
-        """ return device selected interface """
-        reqType = 0x80
-        resp = self.handle.controlMsg(reqType, usb.REQ_GET_CONFIGURATION, 1, value=0x0000, index=0x00)
-        if len(resp) > 0:
-            print "configuration:", resp
-        return resp[0]
-    
+      self.device = usb.core.find(idVendor = POD.VENDOR_ID, idProduct = POD.PRODUCT_ID)
+      if self.device.is_kernel_driver_active(0):
+          try:
+              self.device.detach_kernel_driver(0)
+              print "kernel driver detached"
+          except usb.core.USBError as e:
+              sys.exit("Could not detach kernel driver: %s" % str(e))
+      else:
+          print "no kernel driver attached"
+
     def setguitarmic(self):
         """ set guitar/mic mode """
         try:
             print "g/m:start bulk write"
             buf = [0x18, 0x00, 0x01, 0x00, 0x05, 0x00, 0x0A, 0x40, 0x01, 0x03, 0x00, 0x16, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00]
-            sent = self.handle.bulkWrite(0x01,buf)
-            print "past write", sent
+            sent = self.device.write(POD.BULK_OUT_EP, buf)
         except Exception, e:
-            print "exception in bulkWrite:"
+            print "exception in bulk write:"
             print e
 
     def readData(self, dataLen, addr):
-        resp = self.handle.controlMsg(0x40, 0x67, 0, value=(dataLen << 8) | 0x21, index=addr)
-        print "resp (ACK)", formathex(resp)
-        resp = self.handle.controlMsg(0xC0, 0x67, 1, value=0x12, index=0x00)
-        print "resp (x12)", formathex(resp)
-        resp = self.handle.controlMsg(0xC0, 0x67, dataLen, value=0x13, index=0x00)
-        print "resp (x13)", formathex(resp)
+        resp = self.device.ctrl_transfer(POD.USB_VENDOR_H2D, POD.L6_X3_CTRL, wValue=(dataLen << 8) | 0x21, wIndex=addr, data_or_wLength=0)
+        #print "resp (ACK)", formathex(resp)
+        resp = self.device.ctrl_transfer(POD.USB_VENDOR_D2H, POD.L6_X3_CTRL, wValue=0x12, wIndex=0x00, data_or_wLength=1)
+        #print "resp (x12)", formathex(resp)
+        resp = self.device.ctrl_transfer(POD.USB_VENDOR_D2H, POD.L6_X3_CTRL, wValue=0x13, wIndex=0x00, data_or_wLength=dataLen)
+        #print "resp (x13)", formathex(resp)
         return resp
 
     def init(self):
         """ pod initialization - trying to do the same things the original driver does during X3 startup """
         print "set configuration..."
-        print self.handle.setConfiguration(1)
-        print "---init (20 10 04...)"
-        resp = self.handle.controlMsg(0xC0, 0x67, 3, value=0x11, index=0x00)
-        print "resp:", formathex(resp)
+        cfg = self.device.set_configuration(1)
+        print "-- init (expecting 20 10 04)"
+        resp = self.device.ctrl_transfer(POD.USB_VENDOR_D2H, POD.L6_X3_CTRL, wValue=0x11, wIndex=0x00, data_or_wLength=3)
+        print "resp: %s" % formathex(resp)
         
         print("-- INIT F000...F080")
         #a sequence i don;t understand yet
         bytes = [0x00, 0x08, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38, 0x40, 0x48, 0x50, 0x58, 0x60, 0x68, 0x70, 0x78, 0x80]
         for item in bytes:
             # This looks exactly like $linuxdrv/driver.c/line6_read_data()
-            self.readData(8, 0xF000 | item)
-            resp = self.handle.controlMsg(0x40, 0x67, 0, value=0x0201, index=0x02)
-            print "resp (x201)", formathex(resp)
+            d = self.readData(8, 0xF000 | item)
+            print formathex(d)
+            resp = self.device.ctrl_transfer(POD.USB_VENDOR_H2D, POD.L6_X3_CTRL, wValue=0x0201, wIndex=0x02, data_or_wLength=0)
+            # print "resp (x201)", formathex(resp)
 
-        print "wake up"
-        resp = self.handle.controlMsg(0x00, usb.REQ_SET_FEATURE, 0, value=0x01, index=0x00)
-        print "resp", formathex(resp)
-        print "claim interface 0x1"
-        self.handle.claimInterface(1)
-
-        ##data = [0x0C, 0x00, 0x01, 0x00, 0x02, 0x00, 0x02, 0x40, 0x02, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0xFF]
-        #data = [0x0C, 0x00, 0x01, 0x00, 0x02, 0x00, 0x02, 0x40, 0x02, 0xEE, 0x01, 0x00, 0x00, 0x00, 0x00, 0xFF]
-        #print "write 0x10 bytes to ep 0x01"
-        #resp = self.handle.bulkWrite(0x01, data)
-        #print "resp", resp
-        
-        #print "read 0x40 bytes from ep0x81"
-        #resp = self.handle.bulkRead(0x1, 0x40)
-        #print "resp", formathex(resp)
-
-    def close(self):  
-        """ Release device interface """
-        try:
-            #self.handle.reset()
-            self.handle.releaseInterface()
-        except Exception, err:
-            print >> sys.stderr, err 
+        # TODO: not needed?
+        #print "wake up"
+        #resp = self.device.ctrl_transfer(0x00, usb.REQ_SET_FEATURE, wValue=0x01, wIndex=0x00, data_or_wLength=0)
 
     def setparam(self, paramnum, value_percent):
         """
@@ -170,7 +111,6 @@ class POD():
         maxval = 0x3f800000
         val = (maxval/100) * value_percent
         try:
-            #help(self.handle.bulkWrite)
             buf[0x18] = paramnum
             paramval = struct.pack('i', val)
             buf[0x1c] = ord(paramval[0])
@@ -180,16 +120,16 @@ class POD():
             print 'val=',paramval, 'paramnum=', paramnum
             print formathex(buf)
             print "start bulk write"
-            sent = self.handle.bulkWrite(1,buf,100)
+            sent = self.device.write(POD.BULK_OUT_EP, buf, 100)
             print "past write", sent
-            #sent = self.handle.bulkWrite(0x01,buf,100)
+            #sent = self.device.write(POD.BULK_OUT_EP, buf, 100)
             #print "past write2", sent
             #this message can be see in usb dumps everythime after changing param value,
             #however it appears only once after cranking param up and down constantly
-            resp = self.handle.controlMsg(0xc0, 0x67, 0x03, value=0x11, index=0x00)
+            resp = self.device.ctrl_transfer(POD.USB_VENDOR_D2H, POD.L6_X3_CTRL, wValue=0x11, wIndex=0x00, data_or_wLength=3)
             print "resp", resp
         except Exception, e:
-            print "exception in bulkWrite:",e
+            print "exception in bulk write:",e
 
     def get_serial_number2(self):
         d = self.readData(4, 0x80d0)
@@ -230,6 +170,7 @@ class PacketCompleter(threading.Thread):
         elif data[2] == 0x04:
             if self.curData == []:
                 print("ERROR: Continuing data, but no previous packet stored?!")
+                data = data[:4] # discard the frame
 
         # print "++ ", formathex(data)
         self.curData += data[4:]
@@ -241,10 +182,10 @@ class PacketCompleter(threading.Thread):
         self.curData = []
         self.lastTime = None
 
-        print formathex(data)
+        print formathex(array.array('B', data))
 
 pod = POD()
-pod.open()
+# NOTE: init not needed for the bulk stuff to work
 pod.init()
 pod.get_serial_number2()
 pc = PacketCompleter()
@@ -256,15 +197,15 @@ def signal_handler(signal, frame):
     run = False
 signal.signal(signal.SIGINT, signal_handler)
 
+#pod.setparam(5, 10)
+#pod.setguitarmic()
+
 while run:
     try:
-        resp = pod.handle.bulkRead(0x1, 0x40)
+        resp = pod.device.read(POD.BULK_IN_EP, 0x40)
         pc.appendData(resp)
     except:
         pass
 
 pc.stop = True
-
-#pod.setparam(5, 10)
-#pod.setguitarmic()
-pod.close()
+time.sleep(1)
